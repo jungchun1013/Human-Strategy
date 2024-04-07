295473c2ec43bc8b34135adb6b7db869e2612e99
劉容均 <wendy134577@gmail.com>
2024-04-07 15:52:07 -0400

    pylint check

--- src/main.py
+++ src/main.py
@@ -1,40 +1,25 @@
 import argparse
 import json
 from random import choice, randint, random, choices
-import numpy as np
+import os
 from copy import deepcopy
+import sys
+# import logging
+import math
+import numpy as np
 from pyGameWorld import ToolPicker, objectBoundingBox
 import pymunk as pm
-from pyGameWorld.jsrun import pyGetCollisionsAddForces
-# from mechanism import MechanismGraph
+from pyGameWorld.jsrun import pyGetCollisionsAddForces, pyGetCollisions, pyGetCollisionsPlacement
 from src.strategy import StrategyGraph
 from src.utils import *
-import os
-import logging
-import math
 
-# Load level in from json file
-# For levels used in experiment, check out Level_Definitions/
-json_dir = "./environment/Trials/Strategy/"
-# Basic Table_A
-tnm = "CatapultAlt"
-
-with open(json_dir+tnm+'.json','r') as f:
-  btr0 = json.load(f)
-
-tp0 = ToolPicker(btr0)
-
-movable_obj_dict = {i:j for i,j in tp0.objects.items() if j.color in [(255, 0, 0, 255), (0, 0, 255, 255)]}
-movable_objects = list(movable_obj_dict.keys())
-tools = list(tp0.toolNames)
-available_objects = movable_objects + tools
-
-path_dict0, success, time_to_success = tp0.observeStatePath()
-ext_sampler = ExtrinsicSampler(btr0, path_dict0)
-
-def sample(btr, tp, strategies_graph):
-
-    sample_obj = choices(available_objects, weights=[math.e**(-strategies_graph.obj_count[m]*0.2) for m in available_objects], k=1)[0]
+def sample(args, strategies_graph):
+    tp = args.tp
+    btr = args.btr
+    weights = [ math.e**(-strategies_graph.obj_count[m]*0.2)
+        for m in args.available_objects
+    ]
+    sample_obj = choices(args.available_objects, weights=weights, k=1)[0]
     pos = None
 
 
@@ -43,58 +28,73 @@ def sample(btr, tp, strategies_graph):
     if random() < 0.05 and strategies_graph.full_placement_graphs: # exploit
         graph = choice(strategies_graph.full_placement_graphs)
         obj_list = [nd for nd in graph.nodes() if 'gmm' in graph.nodes[nd]]
-        obj_weight = [1/len(graph.nodes[nd]['ext']) for nd in graph.nodes() if 'gmm' in graph.nodes[nd]]
-        if obj_list: 
+        obj_weight = [ 1/len(graph.nodes[nd]['ext'])
+            for nd in graph.nodes() if 'gmm' in graph.nodes[nd]
+        ]
+        if obj_list:
             print('Exploiting')
             sample_obj = choices(obj_list, weights=obj_weight)[0]
             gmm_sample = graph.nodes[sample_obj]['gmm'].sample()[0][0]
             is_gmm_sample = True
-            if sample_obj in tools:
+            if sample_obj in args.tool_objects:
                 gmm_sample_pos = list(gmm_sample[0:2])
                 print(gmm_sample_pos)
-                path_dict, success, time_to_success = tp.observePlacementStatePath(toolname=sample_obj,position=gmm_sample_pos)
-                path, collisions, end, t = pyGetCollisionsPlacement(tp.world, tp, sample_obj, gmm_sample_pos)
-                init_pose = [gmm_sample_pos[0], gmm_sample_pos[1], 0, 0, 0] 
-                sample_ext = 'tool'     
-                 
+                path_dict, success, time_to_success = tp.observePlacementStatePath(
+                    toolname=sample_obj,
+                    position=gmm_sample_pos
+                )
+                path, collisions, end, _ = pyGetCollisionsPlacement(
+                    tp.world,
+                    tp,
+                    sample_obj,
+                    gmm_sample_pos)
+                init_pose = [gmm_sample_pos[0], gmm_sample_pos[1], 0, 0, 0]
+                sample_ext = 'tool'
+                path_info = path_dict, collisions, success, time_to_success
             else:
                 # btr['world']['objects'][sample_obj]['position'] = gmm_sample[0:2]
                 # btr['world']['objects'][sample_obj]['rotation'] = gmm_sample[2]
                 btr['world']['objects'][sample_obj]['velocity'] = gmm_sample[3:]
                 tp = ToolPicker(btr)
                 path_dict, success, time_to_success = tp.observeStatePath()
-                path, collisions, end, t = pyGetCollisions(tp.world)
+                path, collisions, end, _ = pyGetCollisions(tp.world)
                 init_pose = path_dict[sample_obj][0]
-                sample_ext = 'exploit'     
-            return sample_obj, sample_ext, init_pose, success, path_dict, collisions           
+                sample_ext = 'exploit'
+                path_info = path_dict, collisions, success, time_to_success
+            return sample_obj, sample_ext, init_pose, path_info
 
-
-
-
-
-    if sample_obj in tools:
-        scaled_pos = get_prior_SSUP(tp0) if not is_gmm_sample else gmm_sample[0:2]
-        path_dict, success, time_to_success = tp.observePlacementStatePath(toolname=sample_obj,position=scaled_pos)
-        # path, collisions, end, t = tp.observeFullCollisionEvents(toolname=sample_obj,position=scaled_pos)
-        path, collisions, end, t = pyGetCollisionsPlacement(tp.world, tp, sample_obj, scaled_pos)
+    if sample_obj in args.tool_objects:
+        scaled_pos = get_prior_SSUP(args.tp0) if not is_gmm_sample else gmm_sample[0:2]
+        path_dict, success, time_to_success = tp.observePlacementStatePath(
+            toolname=sample_obj,
+            position=scaled_pos
+        )
+        path, collisions, end, _ = pyGetCollisionsPlacement(
+            tp.world,
+            tp,
+            sample_obj,
+            scaled_pos
+        )
 
         init_pose = [scaled_pos[0], scaled_pos[1], 0, 0, 0]
         sample_ext = 'tool'
+        path_info = path_dict, collisions, success, time_to_success
     else:
         # NOTE - choose what counterfactual extrinsics
         # sample_ext = choice(['pos', 'vel', 'kick'])
-        sample_ext = choice(['vel', 'kick']) 
+        sample_ext = choice(['vel', 'kick'])
         rand_rad = random()*2*np.pi
         # SECTION - sample extrinsics and simulate path
         if sample_ext in ['pos', 'vel']: # fixed pos, smaple from path
             if sample_ext == 'pos':
-                btr = ext_sampler.sample_pos(sample_obj)
+                btr = args.ext_sampler.sample_pos(sample_obj)
             elif sample_ext == 'vel': # set velocity
-                btr = ext_sampler.sample_vel(sample_obj)
+                btr = args.ext_sampler.sample_vel(sample_obj)
             tp = ToolPicker(btr)
             path_dict, success, time_to_success = tp.observeStatePath()
             path, collisions, end, t = pyGetCollisions(tp.world)
             init_pose = path_dict[sample_obj][0]
+            path_info = path_dict, collisions, success, time_to_success
         elif sample_ext == 'kick':
             random_scale = randint(1,50) * 10000
             # sample_obj = 'Lever'
@@ -105,59 +105,80 @@ def sample(btr, tp, strategies_graph):
                 randint(BB[0][0],BB[1][0])-(BB[1][0]-BB[0][0])/2-BB[0][0], \
                 randint(BB[0][1],BB[1][1])-(BB[1][1]-BB[0][1])/2-BB[0][1])
             tp = ToolPicker(btr)
-            force_times = {0.0:[[sample_obj, impulse, pos]], 0.1:[[sample_obj, impulse, pos]], 0.2:[[sample_obj, impulse, pos]]}
-            path_dict, collisions, success, time_to_success = pyGetCollisionsAddForces(tp.world, force_times=force_times)
+            force_times = {
+                0.0:[[sample_obj, impulse, pos]],
+                0.1:[[sample_obj, impulse, pos]],
+                0.2:[[sample_obj, impulse, pos]]
+            }
+            path_info = pyGetCollisionsAddForces(tp.world, force_times=force_times)
+            path_dict, collisions, success, time_to_success = path_info
             init_pose = path_dict[sample_obj][2]
-
     
-    return sample_obj, sample_ext, init_pose, success, path_dict, collisions
+    return sample_obj, sample_ext, init_pose, path_info
 
 def standardize_collisions(collisions):
     for i, c in enumerate(collisions):
         o1, o2, ts, te, ci = c
-        if type(ci[0]) == pm.Vec2d:
+        if isinstance(ci[0], pm.Vec2d):
             ci[0] = [ci[0].x, ci[0].y]
         collisions[i] = [o1, o2, ts, te, ci]
-    return collisions
-        
-    
+    return collisions  
+
 def run(args):
     args.experiment_id = generate_experiment_id()
     args.dir_name = 'data/' + args.experiment_id
     os.makedirs(args.dir_name)
-    CF_run(args)
+
+    # Load level in from json file
+    # For levels used in experiment, check out Level_Definitions/
+    args.json_dir = "./environment/Trials/Strategy/"
+    # Basic Table_A
+    args.tnm = "CatapultAlt"
+
+    with open(args.json_dir + args.tnm + '.json','r') as f:
+        args.btr0 = json.load(f)
+
+    args.tp0 = ToolPicker(args.btr0)
+
+    movable_obj_dict = {i:j for i, j in args.tp0.objects.items()
+                        if j.color in [(255, 0, 0, 255), (0, 0, 255, 255)]
+    }
+    args.movable_objects = list(movable_obj_dict.keys())
+    args.tool_objects = list(args.tp0.toolNames)
+    args.available_objects = args.movable_objects + args.tool_objects
+
+    path_dict0, success, _ = args.tp0.observeStatePath()
+    args.ext_sampler = ExtrinsicSampler(args.btr0, path_dict0)
+    counterfactual_run(args)
 
 
 
-def CF_run(args):
+def counterfactual_run(args):
     # NOTE - MechanismGraph
     strategies_graph = StrategyGraph()
-    # mechanism_graph = MechanismGraph()
     success = False
     image_num = 0
     sample_num = 0
     while not success:
-        btr = deepcopy(btr0)
-        tp = ToolPicker(btr)
+        args.btr = deepcopy(args.btr0)
+        args.tp = ToolPicker(args.btr)
 
-        # FIXED - sample from movable objects and tools
-        
-        sample_obj, sample_ext, init_pose, success, path_dict, collisions = sample(btr, tp, strategies_graph)
-        # print("=============")
+        sample_obj, sample_ext, init_pose, path_info = sample(args, strategies_graph)
+        path_dict, collisions, success, _ = path_info
         collisions = standardize_collisions(collisions)
-        
-        # !SECTION
 
+        # !SECTION
         if success:
-            strategies_graph.build_graph(collisions, sample_obj, init_pose, tp, path_dict, movable_objects)
+            strategies_graph.build_graph(args, sample_obj, init_pose, path_info)
             success = False
             image_num += 1
-    
             # sample
             for g in strategies_graph.full_placement_graphs:
-                if all([('model' in g.nodes[nd]) for nd in g.nodes() if nd not in tp.toolNames]):
+                if all('model' in g.nodes[nd] for nd in g.nodes()
+                    if nd not in args.tp.toolNames
+                ):
                     cur_nd = 'Goal'
-                    gmm = g.nodes[cur_nd]['model'] 
+                    gmm = g.nodes[cur_nd]['model']
                     sample_pos = gmm.sample(n_samples=2)
                     print('Sampled: Goal', sample_pos[0])
                     sample_pos = sample_pos[0]
@@ -165,7 +186,7 @@ def CF_run(args):
                     while True:
                         pred = choice(list(g.predecessors(cur_nd)))
                         cur_nd = pred
-                        if cur_nd in tp.toolNames:
+                        if cur_nd in  args.tp.toolNames:
                             print('Tool:', cur_nd)
                             break
                         gpr = g.nodes[cur_nd]['model']
@@ -175,25 +196,20 @@ def CF_run(args):
                         sample_pos = [item for sublist in sample_pos for item in sublist]
 
                         print('Sampled:', cur_nd, sample_pos)
-                            
-
 
                     sample_obj = cur_nd
                     for s in sample_pos:
                         scaled_pos = list(s[0:2])
-                        path_dict, success, time_to_success = tp.observePlacementStatePath(toolname=sample_obj,position=scaled_pos)
+                        path_dict, success, _ =  args.tp.observePlacementStatePath(
+                            toolname=sample_obj,
+                            position=scaled_pos
+                        )
                         print('Success:', cur_nd, scaled_pos, success)
-                        if success==False:
+                        if success is False:
                             sample_num += 1
                             print('Sample Num:', sample_num)
                         if success:
-                            quit()
-                    
-
-            
-
-
-
+                            sys.exit()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run SSUP')
@@ -205,15 +221,15 @@ if __name__ == "__main__":
     parser.add_argument('--tnm',
                         help='task name', type=int, default=250)
     parser.add_argument('-v', '--verbose',
-                        help='Increase output verbosity', action='store_true')   
+                        help='Increase output verbosity', action='store_true')
     parser.add_argument('--eps',
-                        help='epsilon', type=int, default=0.2)  
+                        help='epsilon', type=int, default=0.2)
     parser.add_argument('--eps-decay-rate',
-                        help='epsilon decay rate', type=int, default=0.95)   
+                        help='epsilon decay rate', type=int, default=0.95)
     parser.add_argument('--max_attempt',
                         help='max number of attempt', type=int, default=200)
     parser.add_argument('--max_sim_attempt',
-                        help='max number of simulated attempt', type=int, default=1000)        
+                        help='max number of simulated attempt', type=int, default=1000)
     args = parser.parse_args()
 
     run(args)
--- src/strategy.py
+++ src/strategy.py
@@ -1,8 +1,7 @@
-
+# strtegy graph class
 
 from collections import Counter
 import networkx as nx
-from copy import deepcopy
 from scipy.stats import norm
 from sklearn import mixture
 from sklearn.gaussian_process import GaussianProcessRegressor
@@ -14,6 +13,10 @@ from src.utils import node_match
 
 
 class StrategyGraph():
+    '''
+    Graph structure for strategy
+    ext: list of extrinsic data (tool, target)
+    '''
     def __init__(self):
         self.path_graphs = []
         self.full_placement_graphs = []
@@ -21,27 +24,27 @@ class StrategyGraph():
         self.obj_count = Counter()
         self.main_graph = nx.DiGraph()
 
-    def build_graph(self, collisions, sample_obj, init_pose, tp, path_dict, available_obj):
+    def build_graph(self, args, sample_obj, init_pose, path_info):
+        path_dict, collisions, success, time_to_success = path_info
         succ_list = []
         succ_list.append(sample_obj)
         graph = nx.DiGraph()
         self.obj_count[sample_obj] += 1
         # add visited flag to collisions
-        collisions = [ c + [False] for c in collisions]
-        
+        collisions = [c + [False] for c in collisions]
+
         while succ_list:
             cur_nd_name = succ_list[0]
-            cur_node = "PLACED" if cur_nd_name in tp.toolNames else cur_nd_name
+            cur_node = "PLACED" if cur_nd_name in args.tp.toolNames else cur_nd_name
             succ_list.pop(0)
             ext_col_list = []
-            pre_ext = None
             if not graph.has_node(cur_nd_name):
                 init_ext = init_pose if cur_node == "PLACED" else path_dict[cur_node][0]
                 graph.add_node(cur_nd_name, ext=[[None, init_ext]])
                 prev_ext = init_pose
             else:
                 prev_ext = graph.nodes[cur_nd_name]['ext'][0][0]
-                
+
             # print(list(zip(visited, collisions)))
             for col in [c for c in collisions if not c[-1]]:
                 is_reverse = False
@@ -60,14 +63,14 @@ class StrategyGraph():
                 else:
                     succ_node = None
                     is_succ = False
-                if (is_succ and succ_node in available_obj 
+                if (is_succ and succ_node in args.available_objects
                     and (succ_node, cur_node) not in graph.edges()):
                     col_time = col[3] if col[3] else col[2]
                     col_end_idx = min(
                         int((col_time+0.1)*10), len(path_dict[succ_node])-1
                     )
                     graph.add_node(
-                        succ_node, 
+                        succ_node,
                         ext=[[prev_ext, path_dict[succ_node][col_end_idx]]]
                     )
                     graph.add_edge(cur_nd_name, succ_node, label=ext_col_list)
@@ -75,22 +78,22 @@ class StrategyGraph():
                     prev_ext = path_dict[succ_node][col_end_idx]
                     succ_list.append(succ_node)
                 elif (is_succ and succ_node == 'Goal'
-                    and (cur_nd_name not in tp.toolNames
-                    and tp.objects[cur_nd_name].color == (255, 0, 0, 255))): # red
+                    and (cur_nd_name not in args.tp.toolNames
+                    and args.tp.objects[cur_nd_name].color == (255, 0, 0, 255))): # red
                     col_end_idx = min(
-                        int((col[2]+0.1)*10), 
+                        int((col[2]+0.1)*10),
                         len(path_dict[cur_node])-1
                     )
                     graph.add_node(
-                        succ_node, 
+                        succ_node,
                         ext=[[path_dict[cur_node][col_end_idx], None]]
                     )
                     graph.add_edge(cur_nd_name, succ_node, label=ext_col_list)
                     ext_col_list=[]
                     break
-                elif is_succ and succ_node not in available_obj:
+                elif is_succ and succ_node not in args.available_objects:
                     ext_col_list.append(succ_node)
-            
+
             if "Goal" not in graph.nodes() and not succ_list:
                 if cur_nd_name == sample_obj:
                     break
@@ -100,7 +103,7 @@ class StrategyGraph():
 
         if "Goal" in graph.nodes():
             print(graph.edges())
-            if sample_obj not in tp.toolNames:
+            if sample_obj not in args.tp.toolNames:
                 self.path_graphs.append(graph)
             else:
                 is_isomorphic = False
@@ -130,9 +133,9 @@ class StrategyGraph():
             print(graph.edges())
             # log
             for node in graph.nodes():
-                if len([d for d in graph.nodes[node]['ext'] if d[0] != None]) > 0:
+                if len([d for d in graph.nodes[node]['ext'] if d[0] is not None]) > 0:
                     data = graph.nodes[node]['ext']
-                    tool_data, target_data = zip(*[d for d in data if d[0] != None])
+                    tool_data, _ = zip(*[d for d in data if d[0] is not None])
                     print(node, len(graph.nodes[node]['ext']), len(tool_data))
                 else:
                     print(node, len(graph.nodes[node]['ext']), '*')
@@ -168,7 +171,7 @@ class StrategyGraph():
                     if len(data) < 2:
                         continue
                     gmm = mixture.GaussianMixture(
-                        n_components=1, 
+                        n_components=1,
                         covariance_type='full'
                     )
                     gmm.fit(data)
@@ -178,19 +181,19 @@ class StrategyGraph():
                     print("==",nd_i, nd_j, "==")
                     data = graph.nodes[nd_j]['ext']
 
-                    tool_data, target_data = zip(*[d for d in data if d[0] != None])
+                    tool_data, target_data = zip(*[d for d in data if d[0] is not None])
                     tool_data = list(tool_data)
                     target_data = list(target_data)
                     if len(tool_data) < 2:
                         continue
-                    X = target_data
+                    x = target_data
                     y = tool_data
     
 
                     kernel = DotProduct() + WhiteKernel()
 
                     # Create the GPR model
-                    gpr = GaussianProcessRegressor(kernel=kernel, random_state=0).fit(X, y)
+                    gpr = GaussianProcessRegressor(kernel=kernel, random_state=0).fit(x, y)
                     graph.nodes[nd_j]['model'] = gpr
                     print('GPR')
                     data = [d[1] for d in graph.nodes[nd_j]['ext']]
@@ -215,6 +218,3 @@ class StrategyGraph():
                     #     mu, std = norm.fit(data_points)
                     #     print(mu, std)
                     #     graph.nodes[nd_j]['ext'] = (mu, std)
-            
-
-
